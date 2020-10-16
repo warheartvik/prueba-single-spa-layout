342083426a9d7186fb844e011e4adb4a9dfc28c8
Joel Denning <joeldenning@gmail.com>
2020-10-16 17:34:37 -0600

    Call Vue.use(Router)

--- root-config/src/quipubyteltda-root-config.js
+++ root-config/src/quipubyteltda-root-config.js
@@ -1,9 +1,13 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
 import { registerApplication, start } from "single-spa";
 import {
   constructApplications,
   constructRoutes,
   constructLayoutEngine,
 } from "single-spa-layout";
+
+Vue.use(VueRouter);
 
 const routes = constructRoutes(document.querySelector("#single-spa-layout"));
 const applications = constructApplications({
